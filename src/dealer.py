e5275847bbc89e9e2f52f4542ac70b546005e53e
lordbobby <zachmcgee99@gmail.com>
2020-04-02 16:45:14 -0400

    dealer fix

--- src/dealer.py
+++ src/dealer.py
@@ -3,12 +3,11 @@ from random import randint
 def deal():
     cards = [0, 0]
 
-    while cards[0] == cards[1]:
+    while cards[1] - cards[0] <= 1:
         cards[0] = randint(1, 10)
         cards[1] = randint(1, 10)
+        cards.sort()
     
     card3 = randint(1, 10)
-    
-    cards.sort()
 
     return (cards, card3)
